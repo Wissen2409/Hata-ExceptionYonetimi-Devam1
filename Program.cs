c1fa8a985f14b18490c446664ffa07c9d5e9bc5a
emrahelis <emrahelis@gmail.com>
2024-10-10 21:24:19 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+try
+{
+
+    Console.WriteLine("1. sayıyı giriniz");
+    int sayi1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("2. sayıyı giriniz");
+    int sayi2 = int.Parse(Console.ReadLine());
+
+    int sonuc = sayi1/sayi2;
+}
+catch (Exception e)
+{
+
+// exceptionların sebeplerini ekrana yazdıralım 
+
+// e. message ile hata mesajının kendine ulaşabiliriz.
+
+Console.WriteLine("Message : "+e.Message);
+// e üzerinden bir çok detaya daha ulaşılabilif
+// örnek hatanın olduğu kod satırını bile hata mesajı üzeirnden ulaşabilirsiniz
+Console.WriteLine("Stack Trace: "+e.StackTrace);
+
+}
